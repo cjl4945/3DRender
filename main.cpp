c55d602538c7b9e9c57135aad1b88338a93954d3
cjl4945 <cjl4945@rit.edu>
2025-02-20 21:46:22 -0500

    Orange cube appears now

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "main.h"
 #include "InitWindow.h"
 #include "Cube.h"
+#include "Shader.h"
 
 using namespace std;
 
@@ -22,6 +23,27 @@ int main(int argc, char** argv) {
 	initGLEW();
 	enableDepthTest();
 
+
+	const char* vertexShaderSource = R"(
+		#version 330 core
+		layout(location = 0) in vec3  aPos;
+		void main() {
+			gl_Position = vec4(aPos, 1.0);
+		}
+	)";
+
+	const char* fragmentShaderSource = R"(
+		#version 330 core
+		out vec4 FragColor;
+		void main() {
+			FragColor = vec4(1.0, 0.5, 0.2, 1.0);
+		}
+	)";
+
+	Shader shader(vertexShaderSource, fragmentShaderSource);
+
+
+
 	Cube cube;
 
 	bool running = true;
@@ -30,6 +52,7 @@ int main(int argc, char** argv) {
 			processEvents(running);
 			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+			shader.use();
 			cube.draw();
 
 			SDL_GL_SwapWindow(window);
