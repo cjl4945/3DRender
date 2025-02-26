f3f907d6e549671278ceeecfdc992e3ec07798bb
cjl4945 <cjl4945@rit.edu>
2025-02-26 13:22:36 -0500

    Finished Resource manager, testing it now

--- ResourceManager.cpp
+++ ResourceManager.cpp
@@ -27,5 +27,44 @@ ResourceData ResourceManager::loadResource(const std::string& path)
 	ResourceData res;
 	res.name = path;
 
-	//
+	//flip image vertically
+	stbi_set_flip_vertically_on_load(true);
+	
+	//load image data
+	res.data = stbi_load(path.c_str(), &res.width, &res.height, &res.channels, 0);
+	if (!res.data)
+	{
+		std::cerr << "Failed to load texture: " << path << std::endl;
+	}
+	
+		return res;
+	}
+
+
+
+
+
+void ResourceManager::loadResourceAsync(const std::string& path)
+{
+	//launch an asynchronous task to load the texture
+	std::future<ResourceData> futureRes = std::async(std::launch::async, &ResourceManager::loadResource, this, path);
+
+	//Wait for resource to load. Look into decoupling this later
+	ResourceData res = futureRes.get();
+	{
+		std::lock_guard<std::mutex> lock(queueMutex);
+		resourceQueue.push(res);
+	}
+
+
+}
+
+bool ResourceManager::getNextResource(ResourceData& resource)
+{
+	std::lock_guard<std::mutex> lock(queueMutex);
+	if (resourceQueue.empty())
+		return false;
+	resource = resourceQueue.front();
+	resourceQueue.pop();
+	return true;
 }
